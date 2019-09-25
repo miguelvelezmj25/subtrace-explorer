f962e38c036df643704622a8e97b246720fe20a8
Miguel Velez <miguelvelez@mijecu25.com>
2019-09-25 10:51:55 -0400

    created maven project

---
+++ src/main/java/edu/cmu/cs/mvelezce/HelloWorld.java
@@ -0,0 +1,8 @@
+package edu.cmu.cs.mvelezce;
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+}
---
+++ src/test/java/edu/cmu/cs/mvelezce/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package edu.cmu.cs.mvelezce;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+  @Test
+  public void main() {
+    HelloWorld.main(new String[0]);
+  }
+}
