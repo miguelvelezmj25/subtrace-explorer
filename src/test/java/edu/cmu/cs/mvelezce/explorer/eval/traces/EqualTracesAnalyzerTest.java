cb7d2c89fc35e88556fc3590b025f942e0755b5a
Miguel Velez <miguelvelez@mijecu25.com>
2019-10-01 15:04:47 -0400

    ran eval to check that we get the same traces for berkeley db

--- src/test/java/edu/cmu/cs/mvelezce/explorer/eval/traces/EqualTracesAnalyzerTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/explorer/eval/traces/EqualTracesAnalyzerTest.java
@@ -3,6 +3,7 @@ package edu.cmu.cs.mvelezce.explorer.eval.traces;
 import edu.cmu.cs.mvelezce.adapter.adapters.measureDiskOrderedScan.BaseMeasureDiskOrderedScanAdapter;
 import edu.cmu.cs.mvelezce.adapter.adapters.trivial.BaseTrivialAdapter;
 import edu.cmu.cs.mvelezce.analysis.dynamic.DynamicAnalysis;
+import edu.cmu.cs.mvelezce.utils.ConfigHelper;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,6 +11,23 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class EqualTracesAnalyzerTest {
+
+  @Test
+  public void trivial_all() throws IOException, InterruptedException {
+    String programName = BaseTrivialAdapter.PROGRAM_NAME;
+    Set<String> options = new HashSet<>(BaseTrivialAdapter.getListOfOptions());
+    Set<Set<String>> configs = ConfigHelper.getConfigurations(options);
+
+    for (Set<String> config : configs) {
+      DynamicAnalysis analysis = new EqualTracesAnalyzer(programName, config);
+
+      String[] args = new String[3];
+      args[0] = "-delres";
+      args[1] = "-saveres";
+      args[2] = "-i 5";
+      analysis.analyze(args);
+    }
+  }
 
   @Test
   public void trivial_1() throws IOException, InterruptedException {
@@ -22,6 +40,23 @@ public class EqualTracesAnalyzerTest {
     args[1] = "-saveres";
     args[2] = "-i 2";
     analysis.analyze(args);
+  }
+
+  @Test
+  public void berkeley_db_all() throws IOException, InterruptedException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    Set<String> options = new HashSet<>(BaseMeasureDiskOrderedScanAdapter.getListOfOptions());
+    Set<Set<String>> configs = ConfigHelper.getConfigurations(options);
+
+    for (Set<String> config : configs) {
+      DynamicAnalysis analysis = new EqualTracesAnalyzer(programName, config);
+
+      String[] args = new String[3];
+      args[0] = "-delres";
+      args[1] = "-saveres";
+      args[2] = "-i 5";
+      analysis.analyze(args);
+    }
   }
 
   @Test
