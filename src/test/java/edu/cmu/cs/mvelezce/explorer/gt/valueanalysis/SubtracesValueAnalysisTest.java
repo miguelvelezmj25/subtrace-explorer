a3c5eb6b3c39efaa023a64e27edd914e43955b17
Miguel Velez <miguelvelez@mijecu25.com>
2019-10-02 11:13:16 -0400

    analyzed the value of subtraces for igen

--- src/test/java/edu/cmu/cs/mvelezce/explorer/gt/valueanalysis/SubtracesValueAnalysisTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/explorer/gt/valueanalysis/SubtracesValueAnalysisTest.java
@@ -1,5 +1,6 @@
 package edu.cmu.cs.mvelezce.explorer.gt.valueanalysis;
 
+import edu.cmu.cs.mvelezce.adapter.adapters.iGen.BaseIGenAdapter;
 import edu.cmu.cs.mvelezce.adapter.adapters.indexFiles.BaseIndexFilesAdapter;
 import edu.cmu.cs.mvelezce.adapter.adapters.measureDiskOrderedScan.BaseMeasureDiskOrderedScanAdapter;
 import edu.cmu.cs.mvelezce.adapter.adapters.trivial.BaseTrivialAdapter;
@@ -17,6 +18,28 @@ public class SubtracesValueAnalysisTest {
   @Test
   public void trivial() throws IOException, InterruptedException {
     String programName = BaseTrivialAdapter.PROGRAM_NAME;
+    SubtraceLabeler subtraceLabeler = new SubtraceLabeler(programName);
+
+    String[] args = new String[0];
+    Map<Set<String>, List<String>> configsToLabeledTraces = subtraceLabeler.analyze(args);
+    SubtracesValueAnalysis subtracesValueAnalysis =
+        new SubtracesValueAnalysis(programName, configsToLabeledTraces);
+
+    args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    Set<SubtraceAnalysisInfo> write = subtracesValueAnalysis.analyze(args);
+
+    subtracesValueAnalysis = new SubtracesValueAnalysis(programName);
+    args = new String[0];
+    Set<SubtraceAnalysisInfo> read = subtracesValueAnalysis.analyze(args);
+
+    Assert.assertEquals(write, read);
+  }
+
+  @Test
+  public void iGen() throws IOException, InterruptedException {
+    String programName = BaseIGenAdapter.PROGRAM_NAME;
     SubtraceLabeler subtraceLabeler = new SubtraceLabeler(programName);
 
     String[] args = new String[0];
