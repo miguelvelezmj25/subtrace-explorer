4d449728a0efdd987a93e9720eb7cb53ffc19296
Miguel Velez <miguelvelez@mijecu25.com>
2019-10-02 13:39:10 -0400

    instrumented subtraces in pngtastic counter

--- src/test/java/edu/cmu/cs/mvelezce/explorer/gt/instrument/SubtracesInstrumenterTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/explorer/gt/instrument/SubtracesInstrumenterTest.java
@@ -1,5 +1,6 @@
 package edu.cmu.cs.mvelezce.explorer.gt.instrument;
 
+import edu.cmu.cs.mvelezce.adapter.adapters.indexFiles.BaseIndexFilesAdapter;
 import edu.cmu.cs.mvelezce.adapter.adapters.measureDiskOrderedScan.BaseMeasureDiskOrderedScanAdapter;
 import edu.cmu.cs.mvelezce.adapter.adapters.pngtastic.BasePngtasticAdapter;
 import edu.cmu.cs.mvelezce.adapter.adapters.trivial.BaseTrivialAdapter;
@@ -52,6 +53,22 @@ public class SubtracesInstrumenterTest {
     String mainClass = BasePngtasticAdapter.MAIN_CLASS;
     String srcDir = BasePngtasticAdapter.INSTRUMENTED_DIR_PATH;
     String classDir = BasePngtasticAdapter.INSTRUMENTED_CLASS_PATH;
+    Instrumenter instrumenter = new SubtracesInstrumenter(programName, mainClass, srcDir, classDir);
+
+    String[] args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    instrumenter.instrument(args);
+  }
+
+  @Test
+  public void instrumentIndexFiles()
+      throws IOException, InterruptedException, NoSuchMethodException, IllegalAccessException,
+          InvocationTargetException {
+    String programName = BaseIndexFilesAdapter.PROGRAM_NAME;
+    String mainClass = BaseIndexFilesAdapter.MAIN_CLASS;
+    String srcDir = BaseIndexFilesAdapter.INSTRUMENTED_DIR_PATH;
+    String classDir = BaseIndexFilesAdapter.INSTRUMENTED_CLASS_PATH;
     Instrumenter instrumenter = new SubtracesInstrumenter(programName, mainClass, srcDir, classDir);
 
     String[] args = new String[2];
