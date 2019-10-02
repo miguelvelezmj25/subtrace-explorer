7aeaa3bd77ea089061825d8dfe1b8cb8d4d775a6
Miguel Velez <miguelvelez@mijecu25.com>
2019-10-02 14:44:16 -0400

    ran value analysis for pngtastic counter

--- src/test/java/edu/cmu/cs/mvelezce/explorer/gt/valueanalysis/SubtracesValueAnalysisTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/explorer/gt/valueanalysis/SubtracesValueAnalysisTest.java
@@ -3,6 +3,7 @@ package edu.cmu.cs.mvelezce.explorer.gt.valueanalysis;
 import edu.cmu.cs.mvelezce.adapter.adapters.iGen.BaseIGenAdapter;
 import edu.cmu.cs.mvelezce.adapter.adapters.indexFiles.BaseIndexFilesAdapter;
 import edu.cmu.cs.mvelezce.adapter.adapters.measureDiskOrderedScan.BaseMeasureDiskOrderedScanAdapter;
+import edu.cmu.cs.mvelezce.adapter.adapters.pngtastic.BasePngtasticAdapter;
 import edu.cmu.cs.mvelezce.adapter.adapters.trivial.BaseTrivialAdapter;
 import edu.cmu.cs.mvelezce.explorer.gt.labeler.SubtraceLabeler;
 import org.junit.Assert;
@@ -57,6 +58,22 @@ public class SubtracesValueAnalysisTest {
     Set<SubtraceAnalysisInfo> read = subtracesValueAnalysis.analyze(args);
 
     Assert.assertEquals(write, read);
+  }
+
+  @Test
+  public void pngtasticCounter() throws IOException, InterruptedException {
+    String programName = BasePngtasticAdapter.PROGRAM_NAME;
+    SubtraceLabeler subtraceLabeler = new SubtraceLabeler(programName);
+
+    String[] args = new String[0];
+    Map<Set<String>, List<String>> configsToLabeledTraces = subtraceLabeler.analyze(args);
+    SubtracesValueAnalysis subtracesValueAnalysis =
+        new SubtracesValueAnalysis(programName, configsToLabeledTraces);
+
+    args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    subtracesValueAnalysis.analyze(args);
   }
 
   @Test
