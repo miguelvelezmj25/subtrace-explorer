2743ad9526b2fe694c02c542f6978e98ef174b1e
Miguel Velez <miguelvelez@mijecu25.com>
2020-04-09 10:32:17 -0400

    ran h2 large with specific configs

--- src/test/java/edu/cmu/cs/mvelezce/explorer/idta/SpecificConfigsIDTATest.java
+++ src/test/java/edu/cmu/cs/mvelezce/explorer/idta/SpecificConfigsIDTATest.java
@@ -1,7 +1,9 @@
 package edu.cmu.cs.mvelezce.explorer.idta;
 
+import edu.cmu.cs.mvelezce.adapters.convert.BaseConvertAdapter;
 import edu.cmu.cs.mvelezce.adapters.indexFiles.BaseIndexFilesAdapter;
 import edu.cmu.cs.mvelezce.adapters.measureDiskOrderedScan.BaseMeasureDiskOrderedScanAdapter;
+import edu.cmu.cs.mvelezce.adapters.runBenchC.BaseRunBenchCAdapter;
 import edu.cmu.cs.mvelezce.adapters.trivial.BaseTrivialAdapter;
 import edu.cmu.cs.mvelezce.explorer.idta.config.ConfigAnalysis;
 import edu.cmu.cs.mvelezce.explorer.idta.config.IDTAConfigAnalysis;
@@ -56,14 +58,109 @@ public class SpecificConfigsIDTATest {
   }
 
   @Test
+  public void indexFilesSmall() throws IOException, InterruptedException {
+    String programName = BaseIndexFilesAdapter.PROGRAM_NAME;
+    SpecificConfigsGetter specificConfigsGetter = new SpecificConfigsGetter(programName);
+    String[] args = new String[0];
+    Set<Set<String>> configs = specificConfigsGetter.analyze(args);
+
+    List<String> options = BaseIndexFilesAdapter.getListOfOptions();
+    IDTA analysis = new SpecificConfigsIDTA(programName, "small", options, configs);
+
+    args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    analysis.analyze(args);
+  }
+
+  @Test
+  public void ConvertSmall() throws IOException, InterruptedException {
+    String programName = BaseConvertAdapter.PROGRAM_NAME;
+    SpecificConfigsGetter specificConfigsGetter = new SpecificConfigsGetter(programName);
+    String[] args = new String[0];
+    Set<Set<String>> configs = specificConfigsGetter.analyze(args);
+
+    List<String> options = BaseConvertAdapter.getListOfOptions();
+    IDTA analysis = new SpecificConfigsIDTA(programName, "small", options, configs);
+
+    args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    analysis.analyze(args);
+  }
+
+  @Test
+  public void RunBenchCSmall() throws IOException, InterruptedException {
+    String programName = BaseRunBenchCAdapter.PROGRAM_NAME;
+    SpecificConfigsGetter specificConfigsGetter = new SpecificConfigsGetter(programName);
+    String[] args = new String[0];
+    Set<Set<String>> configs = specificConfigsGetter.analyze(args);
+
+    List<String> options = BaseRunBenchCAdapter.getListOfOptions();
+    IDTA analysis = new SpecificConfigsIDTA(programName, "small", options, configs);
+
+    args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    analysis.analyze(args);
+  }
+
+  @Test
+  public void MeasureDiskOrderedScanLarge() throws IOException, InterruptedException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    SpecificConfigsGetter specificConfigsGetter = new SpecificConfigsGetter(programName);
+    String[] args = new String[0];
+    Set<Set<String>> configs = specificConfigsGetter.analyze(args);
+
+    List<String> options = BaseMeasureDiskOrderedScanAdapter.getListOfOptions();
+    IDTA analysis = new SpecificConfigsIDTA(programName, "large", options, configs);
+
+    args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    analysis.analyze(args);
+  }
+
+  @Test
   public void indexFilesLarge() throws IOException, InterruptedException {
     String programName = BaseIndexFilesAdapter.PROGRAM_NAME;
-    List<String> options = BaseIndexFilesAdapter.getListOfOptions();
-
-    ConfigAnalysis configAnalysis = new IDTAConfigAnalysis(programName, "small");
+    SpecificConfigsGetter specificConfigsGetter = new SpecificConfigsGetter(programName);
     String[] args = new String[0];
-    Set<Set<String>> configs = configAnalysis.analyze(args);
+    Set<Set<String>> configs = specificConfigsGetter.analyze(args);
 
+    List<String> options = BaseIndexFilesAdapter.getListOfOptions();
+    IDTA analysis = new SpecificConfigsIDTA(programName, "large", options, configs);
+
+    args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    analysis.analyze(args);
+  }
+
+  @Test
+  public void ConvertLarge() throws IOException, InterruptedException {
+    String programName = BaseConvertAdapter.PROGRAM_NAME;
+    SpecificConfigsGetter specificConfigsGetter = new SpecificConfigsGetter(programName);
+    String[] args = new String[0];
+    Set<Set<String>> configs = specificConfigsGetter.analyze(args);
+
+    List<String> options = BaseConvertAdapter.getListOfOptions();
+    IDTA analysis = new SpecificConfigsIDTA(programName, "large", options, configs);
+
+    args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    analysis.analyze(args);
+  }
+
+  @Test
+  public void RunBenchCLarge() throws IOException, InterruptedException {
+    String programName = BaseRunBenchCAdapter.PROGRAM_NAME;
+    SpecificConfigsGetter specificConfigsGetter = new SpecificConfigsGetter(programName);
+    String[] args = new String[0];
+    Set<Set<String>> configs = specificConfigsGetter.analyze(args);
+
+    List<String> options = BaseRunBenchCAdapter.getListOfOptions();
     IDTA analysis = new SpecificConfigsIDTA(programName, "large", options, configs);
 
     args = new String[2];
